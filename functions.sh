634cb606953b89942558a90bc47e7a6af3109cbd
Francisco Calle Moreno <francisco.calle.moreno@gmail.com>
2021-10-29 10:04:16 +0200

    feat: colors

--- functions.sh
+++ functions.sh
@@ -28,3 +28,9 @@ clearDocker() {
   docker network rm $(docker network ls -q)
   docker volume rm $(docker volume ls -q)
 }
+
+printColors() {
+    for i in {0..255}; do 
+        print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; 
+    done
+}
