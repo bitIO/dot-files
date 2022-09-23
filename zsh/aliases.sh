932642ae6e69059b8974e924cdc5b928039d8869
Francisco Calle Moreno <francisco.calle.moreno@gmail.com>
2022-09-23 17:59:01 +0200

    fix(zsh): updated plugins list
    Added zsh-safe-rm
    https://github.com/mattmc3/zsh-safe-rm

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -3,11 +3,13 @@ if [ -x "$(command -v exa)" ]; then
     alias la="ls --long --all --group"
 fi
 
-alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
+
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-alias wget='wget --no-check-certificate'
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+
 alias resetSpotlight='sudo mdutil -a -i off;sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist;sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist;sudo mdutil -a -i on'
 
-alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
+alias wget='wget --no-check-certificate'
 
 alias vscode="code --user-data-dir ~/Dev/vscode/valassis/data --extensions-dir ~/Dev/vscode/valassis/extensions"
