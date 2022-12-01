1e87466eb117019588c503c32313b14abd7192e5
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-12-01 12:05:20 +0100

    feat(node): check for yarn

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # My dot-files
 
 You know, everyone has his own ones 😅👴🏼
+
+## Node
+
+Recommended packages to install:
+
+- [yarn](https://yarnpkg.com/)
+- [ntl](https://github.com/ruyadorno/ntl)
--- zsh/node.sh
+++ zsh/node.sh
@@ -1,3 +1,9 @@
-if [ "$(which ntl)" != "" ]; then
+if [ "$(which yarn)" = "" ]; then
+  echo "🔥 [NODE] yarn not installed"
+fi
+
+if [ "$(which ntl)" = "" ]; then
+  echo "🔥 [NODE] ntl not installed"
+else
   export NTL_RUNNER=yarn
 fi
