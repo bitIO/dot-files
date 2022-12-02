87efbd0b539afea47fbcc1a9d5cd64e31de74d7e
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-12-02 10:46:32 +0100

    fix(node): check for node-check-updates

--- zsh/node.sh
+++ zsh/node.sh
@@ -1,8 +1,13 @@
-if [ "$(which yarn)" = "" ]; then
+if [ -z "$(command -v yarn)" ]; then
   echo "🔥 [NODE] yarn not installed"
 fi
 
-if [ "$(which ntl)" = "" ]; then
+if [ -z "$(command -v ncu)" ]; then
+  echo "🔥 [NODE] node-check-updates not installed"
+  echo "ℹ️  npm install -g npm-check-updates"
+fi
+
+if [ -z "$(command -v ntl)" ]; then
   echo "🔥 [NODE] ntl not installed"
 else
   export NTL_RUNNER=yarn
