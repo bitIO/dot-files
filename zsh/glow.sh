ccc06396f88de49229c1341472d7f98a52a24a76
Francisco Calle <francisco.calle.moreno@gmail.com>
2025-01-22 19:56:51 +0100

    docs(zsh): add description on glow

--- zsh/glow.sh
+++ zsh/glow.sh
@@ -1,3 +1,4 @@
+# Render markdown on the CLI, with syntax highlighting
 if [ ! -x "$(command -v glow)" ]; then
   echo "Glow markdown viewer not found. Install with brew"
   echo "brew install glow"
