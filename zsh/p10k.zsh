cf7a657ed389285cfd5b455848e257ddc7877aca
Francisco Calle <francisco.calle.moreno@gmail.com>
2025-01-22 19:39:53 +0100

    fix(zsh): enable kubecontext

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -76,7 +76,7 @@
     #phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
     #scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
     #haskell_stack           # haskell version from stack (https://haskellstack.org/)
-    #kubecontext             # current kubernetes context (https://kubernetes.io/)
+    kubecontext             # current kubernetes context (https://kubernetes.io/)
     #terraform               # terraform workspace (https://www.terraform.io)
     # terraform_version     # terraform version (https://www.terraform.io)
     #toolbox                 # toolbox name (https://github.com/containers/toolbox)
