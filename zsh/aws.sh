bdc218fbcd693cff66edd45c9ba7f9aa2fcd0c46
Francisco Calle <francisco.calle.moreno@gmail.com>
2025-02-13 15:58:56 +0100

    fix(zsh): remove aws-vault

--- zsh/aws.sh
+++ zsh/aws.sh
@@ -1,7 +1,1 @@
-ZSH_AWSVAULT="$ZSH_CUSTOM/plugins"
-if [ ! -d $ZSH_AWSVAULT/zsh-aws-vault ]; then
-  git clone --depth 1 https://github.com/blimmer/zsh-aws-vault.git \
-    $ZSH_AWSVAULT/zsh-aws-vault
-fi
-
 source $DOT_FILES_REPO/aws/zshrc
