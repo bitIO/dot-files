1d2d44ea62400a06ef5d0cde9e003da3ba5afe2a
Francisco <francisco.calle.moreno@gmail.com>
2025-05-19 13:58:46 +0200

    fix: centralize compinit

--- zsh/fzf.sh
+++ zsh/fzf.sh
@@ -3,6 +3,5 @@ if [ ! -x "$(command -v fzf)" ]; then
   echo "brew install fzf"
 elif [ -f ~/.fzf.zsh ]; then
   source ~/.fzf.zsh
-  autoload -U compinit && compinit
   zinit light Aloxaf/fzf-tab # fzf tab completion
 fi
